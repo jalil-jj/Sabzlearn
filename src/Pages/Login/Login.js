c97afa8cfaff740c3a24dbf4039eaffbc17c6c6b
jalil <jalil1379jaberi@gmail.com>
2025-01-25 12:26:53 -0800

    UseReduce for Inputs

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import Footer from "../../Components/Footer/Footer";
 import Navbar from "../../Components/Navbar/Navbar";
 import Topbar from "../../Components/Topbar/Topbar";
+import Input from '../../Components/Form/Input'
 
 import "./Login.css";
 
@@ -26,18 +27,20 @@ export default function Login() {
           </div>
           <form action="#" class="login-form">
             <div class="login-form__username">
-              <input
-                class="login-form__username-input"
+              <Input
+                element='input'
+                className='login-form__username-input'
                 type="text"
                 placeholder="نام کاربری یا آدرس ایمیل"
               />
               <i class="login-form__username-icon fa fa-user"></i>
             </div>
             <div class="login-form__password">
-              <input
-                class="login-form__password-input"
+              <Input
+                className="login-form__password-input"
                 type="text"
                 placeholder="رمز عبور"
+                element='input'
               />
               <i class="login-form__password-icon fa fa-lock-open"></i>
             </div>
