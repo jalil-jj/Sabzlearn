6895eaee2899b22cb13c4304f7789a63514bb3a7
jalil <jalil1379jaberi@gmail.com>
2025-01-25 06:53:35 -0800

    Courses / Create loginPage and RegisterPage

---
+++ src/Pages/Courses/Courses.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import Topbar from '../../Components/Topbar/Topbar'
+import Navbsr from '../../Components/Navbar/Navbar'
+import Breadcrumb from '../../Components/Breadcrumb/Breadcrumb'
+import CourseBox from '../../Components/CourseBox/CourseBox'
+import Footer from '../../Components/Footer/Footer'
+
+import './Courses.css'
+
+export default function Courses() {
+    return (
+
+        <>
+            <Topbar />
+            <Navbsr />
+
+            <Breadcrumb
+                links={[
+                    { id: 1, title: "خانه", to: '' },
+                    { id: 2, title: "آخرین دوره ها", to: 'corses' },
+                ]}
+            />
+
+            {/* <!--------------------------------  Courses-Section  --------------------------------> */}
+            <section class="courses">
+                <div class="container">
+                    <div class="courses-content">
+                        <div class="container">
+                            <div class="row">
+                                <CourseBox />
+                                <CourseBox />
+                                <CourseBox />
+                                <CourseBox />
+                                <CourseBox />
+                                <CourseBox />
+                                <CourseBox />
+                                <CourseBox />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="courses-pagination">
+                        <ul class="courses__pagination-list">
+                            <li class="courses__pagination-item">
+                                <a href="#" class="courses__pagination-link">
+                                    <i class="fas fa-long-arrow-alt-right courses__pagination-icon"></i>
+                                </a>
+                            </li>
+                            <li class="courses__pagination-item">
+                                <a
+                                    href="#"
+                                    class="courses__pagination-link courses__pagination-link--active"
+                                >
+                                    1
+                                </a>
+                            </li>
+                            <li class="courses__pagination-item">
+                                <a href="#" class="courses__pagination-link">
+                                    2
+                                </a>
+                            </li>
+                            <li class="courses__pagination-item">
+                                <a href="#" class="courses__pagination-link">
+                                    3
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            {/* <!--------------------------------  Courses-Section  --------------------------------> */}
+
+            <Footer />
+        </>
+
+
+    )
+}
